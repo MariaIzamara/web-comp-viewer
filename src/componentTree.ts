03d00bd5283b8554fdb0e1882d47986d297fd6f5
Maria Izamara Coutinho <mariaizamaracoutinho@gmail.com>
2024-11-18 10:25:27 -0300

    Feat: Initial setup with activity bar icon, docs.json integration, welcome view and tree-view (#1)
    * stencil file read
    
    * Initial implementation of getChildren()
    
    * Finished TreeDataProvider
    
    * Added viewsWelcome
    
    * Added node icon and console adjustments
    
    * Added console.log and formatted files

---
+++ src/componentTree.ts
@@ -0,0 +1,213 @@
+import * as vscode from 'vscode';
+import ts from 'typescript';
+import * as fs from 'fs';
+import * as fspath from 'path';
+
+const STENCIL_CONFIG_FILE_NAME = 'stencil.config.ts';
+const DOCS_JSON_FILE_NAME = 'docs.json';
+
+type Component = {
+    tag: string;
+    docs: string;
+    dependents: string[];
+    dependencies: string[];
+};
+
+type DocsJson = {
+    components: Component[];
+}
+
+export class ComponentTreeDataProvider implements vscode.TreeDataProvider<Node> {
+    private rootNodes: Node[] = [];
+
+    private _onDidChangeTreeData: vscode.EventEmitter<Node | undefined | void> = new vscode.EventEmitter<Node | undefined | void>();
+    readonly onDidChangeTreeData: vscode.Event<Node | undefined | void> = this._onDidChangeTreeData.event;
+
+    constructor(private path: string | undefined) {
+    }
+
+    refresh(path: string | undefined): void {
+        this.path = path;
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: Node): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element;
+    }
+
+    getChildren(element?: Node): vscode.ProviderResult<Node[]> {
+        if (!this.path) {
+            console.log('[ComponentTreeDataProvider] No defined path.');
+            this.setDocsJsonNotFound();
+            return;
+        }
+
+        const docsJson = this.getDocsJson(this.path);
+
+        if (element) {
+            return this.getElementNodesFromRootNodes(this.rootNodes, element);
+        } else {
+            this.rootNodes = this.getRootNodesFromDocsJson(docsJson);
+            return this.rootNodes;
+        }
+    }
+
+    private getElementNodesFromRootNodes(root: Node[], element: Node): Node[] {
+        const dependencies = element.dependencies;
+
+        return dependencies.flatMap(dependency => {
+            const component = root.find(component => component.tag === dependency);
+
+            if (!component) {
+                return [];
+            }
+
+            const { tag, docs, dependents, dependencies } = component;
+            const collapsibleState = dependencies.length > 0
+                ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
+
+            return [new Node(tag, docs, dependents, dependencies, collapsibleState)];
+        });
+    }
+
+    private getRootNodesFromDocsJson(docsJson: DocsJson | undefined): Node[] {
+        if (!docsJson) {
+            return [];
+        }
+
+        const components = docsJson.components;
+
+        return components.map(component => {
+            const { tag, docs, dependents, dependencies } = component;
+            const filteredDependencies = dependencies.filter(dependency => components.find(component => component.tag === dependency));
+            const collapsibleState = filteredDependencies.length > 0
+                ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None;
+
+            return new Node(tag, docs, dependents, filteredDependencies, collapsibleState);
+        });
+    }
+
+    private getDocsJson(path: string): DocsJson | undefined {
+        try {
+            if (!path.endsWith(DOCS_JSON_FILE_NAME)) {
+                const stencilConfigPath = fspath.join(path, STENCIL_CONFIG_FILE_NAME);
+                const docsJsonPath = this.getDocsJsonPath(stencilConfigPath);
+                if (!docsJsonPath) {
+                    throw Error('Path not found.');
+                }
+                path = fspath.join(path, docsJsonPath);
+            }
+            if (this.pathExists(path)) {
+                return JSON.parse(fs.readFileSync(path, 'utf-8'));
+            } else {
+                throw Error('File not found.');
+            }
+        } catch (error) {
+            console.error(`[${DOCS_JSON_FILE_NAME}] ${error}`);
+            this.setDocsJsonNotFound();
+        }
+    }
+
+    private getDocsJsonPath(stencilConfigPath: string): string | undefined {
+        try {
+            if (this.pathExists(stencilConfigPath)) {
+                const stencilConfigFile = fs.readFileSync(stencilConfigPath, 'utf-8');
+                const stencilConfigSourceFile = ts.createSourceFile(STENCIL_CONFIG_FILE_NAME, stencilConfigFile, ts.ScriptTarget.ESNext, false);
+
+                return this.findDocsJsonPath(stencilConfigSourceFile);
+            } else {
+                throw Error('File not found.');
+            }
+        } catch (error) {
+            console.error(`[${STENCIL_CONFIG_FILE_NAME}] ${error}`);
+        }
+    }
+
+    private findDocsJsonPath(stencilConfigSourceFile: ts.SourceFile): string {
+        let docsJsonPath: string = '';
+
+        stencilConfigSourceFile.forEachChild(node => {
+            if (
+                ts.isVariableStatement(node) &&
+                node.declarationList.declarations.length > 0
+            ) {
+                const declaration = node.declarationList.declarations[0];
+
+                if (
+                    ts.isIdentifier(declaration.name) &&
+                    declaration.name.text === 'config' &&
+                    declaration.initializer &&
+                    ts.isObjectLiteralExpression(declaration.initializer)
+                ) {
+                    const outputTargetsProperty = declaration.initializer.properties.find(prop =>
+                        ts.isPropertyAssignment(prop) &&
+                        ts.isIdentifier(prop.name) &&
+                        prop.name.text === 'outputTargets'
+                    ) as ts.PropertyAssignment | undefined;
+
+                    if (outputTargetsProperty && ts.isArrayLiteralExpression(outputTargetsProperty.initializer)) {
+                        const outputTargetsArray = outputTargetsProperty.initializer.elements;
+
+                        for (const element of outputTargetsArray) {
+                            if (ts.isObjectLiteralExpression(element)) {
+                                const typeProperty = element.properties.find(prop =>
+                                    ts.isPropertyAssignment(prop) &&
+                                    ts.isIdentifier(prop.name) &&
+                                    prop.name.text === 'type' &&
+                                    ts.isStringLiteral(prop.initializer) &&
+                                    prop.initializer.text === 'docs-json'
+                                );
+
+                                if (typeProperty) {
+                                    element.properties.forEach(prop => {
+                                        if (ts.isPropertyAssignment(prop) && ts.isIdentifier(prop.name) && prop.name.text === 'file') {
+                                            docsJsonPath = ts.isStringLiteral(prop.initializer)
+                                                ? prop.initializer.text
+                                                : prop.initializer.getText();
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        return docsJsonPath;
+    }
+
+    private pathExists(path: string): boolean {
+        try {
+            fs.accessSync(path);
+        } catch {
+            return false;
+        }
+
+        return true;
+    }
+
+    private setDocsJsonNotFound() {
+        vscode.commands.executeCommand('setContext', 'extension.docsJsonNotFound', true);
+    }
+}
+
+export class Node extends vscode.TreeItem {
+    constructor(
+        public readonly tag: string,
+        public readonly docs: string,
+        public readonly dependents: string[],
+        public readonly dependencies: string[],
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    ) {
+        super(tag, collapsibleState);
+
+        this.label = this.tag;
+        this.tooltip = this.docs;
+    }
+
+    iconPath = {
+        light: fspath.join(__filename, '..', '..', 'assets', 'light', 'node.svg'),
+        dark: fspath.join(__filename, '..', '..', 'assets', 'dark', 'node.svg')
+    };
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/**
+ * @introduction main file
+ *
+ * @description application will start from this file
+ */
 import * as vscode from 'vscode';
+import { ComponentTreeDataProvider } from './componentTree';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "web-comp-analyzer" is now active!');
+	const componentTreeDataProvider = new ComponentTreeDataProvider(rootPath);
+	vscode.window.registerTreeDataProvider('tree-view', componentTreeDataProvider);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('web-comp-analyzer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
-	});
+	const openFile = vscode.commands.registerCommand(
+		"extension.openFile",
+		async () => {
+			const fileUri = await vscode.window.showOpenDialog({
+				canSelectMany: false,
+				openLabel: 'Open',
+				filters: { 'JSON Files': ['json'] }
+			});
 
-	context.subscriptions.push(disposable);
+			let docsJsonPath: string | undefined;
+
+			if (fileUri && fileUri[0]) {
+				await vscode.commands.executeCommand('vscode.open', fileUri[0]);
+				docsJsonPath = fileUri[0].path.replaceAll('/', '\\').slice(1);
+			} else {
+				console.log('[extension.openFile] No files were selected.');
+			}
+			componentTreeDataProvider.refresh(docsJsonPath);
+		}
+	);
+
+	context.subscriptions.push(openFile);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
